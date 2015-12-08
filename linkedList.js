663143bdae9aa0ac9ceaf734a02b1bec522c575b
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-12-07 20:09:38 -1000

    saved linkedListGenerator() in to var one

--- linkedList.js
+++ linkedList.js
@@ -4,54 +4,83 @@
 //  * @return {Object} an object exposing methods to be used to manipulate a linked list
 //  */
 function linkedListGenerator(){
-  var node = {};
+
   var head = null;
   var tail = null;
-
-  return {
-    getHead:function(){
-      return head;
-    },
-
-    getTail:function(){
-      return tail;
-    },
-
-    add:function(value){
-      var newNodeObject={
-        value:value,
-        next:null,
-      };
-
-      if(head===null&&tail===null){
-        head=newNodeObject;
-        tail=newNodeObject;
-        return newNodeObject;
-      }
-      else{
-        tail.next=newNodeObject;
-        tail=newNodeObject;
-        return newNodeObject;
-      }
-    },
-
-    get:function(number){
-
-    },
-
-    remove:function(number){
-
-    },
+  var test = 0;
+  var module = {};
 
 
-    insert:function(Value,number){
+  module.getHead = function(){
+    return head;
+  };
 
+  module.getTail = function(){
+    return tail;
+  };
+
+  module.add = function(parameter){
+    var newNodeObject={
+      value:parameter,
+      next:null,
+    };
+      test = 'testing';
+
+    if(head===null&&tail===null){
+      head=newNodeObject;
+      tail=newNodeObject;
+      console.log(head,'head');
+      return newNodeObject;
     }
+    else{
+      tail.next=newNodeObject;
+      tail=newNodeObject;
+      return newNodeObject;
+    }
+  };
+
+  module.get = function(number){
+    var i=1;
+    if(number===0){
+     return linkedListGenerator().getHead();
+    }
+      var currentNode = head;
+      console.log(test);
+      console.log(head);
+      while(i<number){
+        currentNode = currentNode.next;
+        i++;
+      }
+      return head;
+  };
+
+  module.remove = function(number){
 
   };
 
 
+  module.insert = function(Value,number){
+
+  };
+
+  return module;
 }
+
+
+// console.log(linkedListGenerator().getHead());
+var one = linkedListGenerator();
+console.log(one.add('bomber'));
+console.log(one.getHead());
+
+console.log(one.add('man'));
+console.log(one.add('Playstation'));
+console.log(one.add('console'));
+
+console.log(one.get(2));
+
+
+
+
 
 
 
